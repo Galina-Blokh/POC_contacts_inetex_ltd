3870e099aa0da8d804e268a075ff93b38573a99b
GITHUB USERNAME <GITHUB EMAIL>
2019-06-13 22:22:41 +0300

    added entity PbAddress

---
+++ src/main/java/com/inetexltd/mycontacts/app/model/entity/PbAddress.java
@@ -0,0 +1,24 @@
+package com.inetexltd.mycontacts.app.model.entity;
+
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter @Setter
+@EqualsAndHashCode
+@Table(name = "addresses")
+public class PbAddress extends PbSuperUser {
+    @Column(name = "country")
+    private  String country;
+    @Column(name = "city")
+    private  String city;
+    @Column(name = "house_number")
+    private String houseNumber;
+    @Column(name = "apartment")
+    private String apartment;
+}
